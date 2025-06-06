0fab92bcf234c561bf2f1b9a8821e0f14ee4f368
soiqualang <soiqualang.chentreu@gmail.com>
2025-06-06 23:45:46 +0000

    update thuc hanh

---
+++ ThucHanh/Buoi2_php_sql/php_sql.php
@@ -0,0 +1,45 @@
+<?php
+	//Tạo connection
+	define("PG_DB","gis_db");
+	define("PG_HOST","localhost");
+	define("PG_USER","postgres");
+	define("PG_PORT","5433");
+	define("PG_PASS","*******");
+	
+	$dbcon=pg_connect("dbname=".PG_DB." user=".PG_USER." password=".PG_PASS." host=".PG_HOST." port=".PG_PORT);
+	
+	//SELECT
+	//Tạo câu truy vấn
+	$sql='SELECT * FROM ds_hocsinh';
+	
+	//Thực thi truy vấn
+	$query=pg_query($dbcon,$sql);
+	
+	while($kq=pg_fetch_assoc($query)){
+		echo 'Tên: '.$kq['ten'].' - '.$kq['tuoi'].' - '.$kq['diachi'];
+		echo '<br>';
+	}
+	
+	//Insert
+	//Tạo truy vấn
+	$sql="INSERT INTO ds_hocsinh (ten, tuoi, diachi)
+VALUES ('Phạm B', '12', 'dsfdsgsg')";
+	//thực thi
+	//pg_query($dbcon,$sql);
+	
+	//Update - Cập nhật dữ liệu
+	$sql="UPDATE ds_hocsinh SET
+ten = 'Phạm Văn B',
+tuoi = '17',
+diachi = 'Ấp C'
+WHERE id = '8'";
+
+	//pg_query($dbcon,$sql);
+	
+	//Delete - Xóa dữ liệu
+	$sql="DELETE FROM ds_hocsinh
+WHERE id = '2'";
+	//pg_query($dbcon,$sql);
+	
+	
+?>
